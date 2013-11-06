c5bfa63c99cb5ebc932853934e53fb1287630e16
Rustam Zagirov <rustam@zagirov.name>
2013-11-06 15:04:48 +0400

    fresh rails 4.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geo::Application.config.secret_key_base = '4b08f8ba09ed310651db7001f1965ddc67752cebf164eb33ae704e9731e1f3d0e33d7ecfb3ef6e5e2f2aa2cb6ecdd1a6b3c1b7837de0048b5e021757e34b0610'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geo::Application.config.session_store :cookie_store, key: '_geo_session'
