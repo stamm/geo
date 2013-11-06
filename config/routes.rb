b94914b6a11b0d259d0068bb0bb89b2cfa90f3ac
Rustam Zagirov <rustam@zagirov.name>
2013-11-06 15:20:36 +0400

    generate map controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Geo::Application.routes.draw do
+  root 'map#index'
+  get 'map/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
